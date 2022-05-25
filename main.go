9cfa49aafd8623c9acbe328c77a5713534a701a1
Avik Kundu <1828008@kiit.ac.in>
2022-05-25 12:54:13 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World v4!")
+	})
+	if os.Getenv("PORT") != "" {
+		port := os.Getenv("PORT")
+		fmt.Printf("Server running (port=%s), route: http://localhost:%s/\n", port, port)
+		log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+	} else {
+		fmt.Printf("Server running (port=8080), route: http://localhost:8080/\n")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+}
